0dcbb77ab6d3959f056124b5648969c7e8e700de
Vitor Souza <vitorlegalvh@gmail.com>
2025-11-23 20:55:20 -0300

    feat(jump): adicionando coyote time na classe jogador

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,9 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 var is_dead: bool = false
+var can_jump: bool = false
+
+@onready var coyote_timer: Timer = $CoyoteTimer
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
@@ -14,16 +17,24 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY
-			animated_sprite.play("jump")
-		elif direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
-	else:
+		can_jump = true
+		coyote_timer.stop()
+	else: 
 		velocity += get_gravity() * delta
 
+	if Input.is_action_just_pressed("jump") and can_jump:
+		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("jump")
+		can_jump = false
+		coyote_timer.start()
+	elif direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
+
 	move_and_slide()
+
+func _on_coyote_timer_timeout():
+	can_jump = false
